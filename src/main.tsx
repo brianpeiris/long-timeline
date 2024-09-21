1d1c6548268bc5f0f8a7f51372a56be41e7e0566
Brian Peiris <79419+brianpeiris@users.noreply.github.com>
2024-09-20 21:44:28 -0400

    inital commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+
+import { Box } from './Box'
+
+createRoot(document.getElementById('root')!).render(
+  <Canvas>
+    <ambientLight intensity={Math.PI / 2} />
+    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+    <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+    <Box position={[-1.2, 0, 0]} />
+    <Box position={[1.2, 0, 0]} />
+  </Canvas>,
+)
